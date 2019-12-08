30bd7dc5acea8c2c91f6f9ba66ab0a964931ec40
Jessica Gibbins <jessagibbins@gmail.com>
2019-12-08 09:40:34 -0500

    Add initial css

--- app.js
+++ app.js
@@ -16,15 +16,16 @@ fetch(url)
         return employees.map(function(employee) {
             let li = createNode('li'),
                 img = createNode('img'),
-                span = createNode('span');
+                h2 = createNode('h2');
                 a = createNode('a');
                 p = createNode('p');
+            li.classList.add('employee');
             img.src = employee.picture.medium;
-            span.innerHTML = `${employee.name.first} ${employee.name.last}`;
+            h2.innerHTML = `${employee.name.first} ${employee.name.last}`;
             a.innerHTML = `${employee.email}`;
             p.innerHTML = `${employee.location.city}`;
             append(li, img);
-            append(li, span);
+            append(li, h2);
             append(li, a);
             append(li, p);
             append(ul, li);
