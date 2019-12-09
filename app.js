8be2814b94ce9edb08ef6e62f2b9e8c1f16a0278
Jessica Gibbins <jessagibbins@gmail.com>
2019-12-08 19:11:29 -0500

    Add functionality

--- app.js
+++ app.js
@@ -1,36 +1,101 @@
-const ul = document.getElementById('employees'); 
+// Variables
+
+const employeeList = document.getElementById('employee-list'); 
 const url = 'https://randomuser.me/api/?results=12';
+const list = [];
+let index = 0;
+const modal = document.createElement('div');
+const closeBtn = document.createElement('span');
+const modalContainer = document.createElement('div');
 
-function createNode(element) {
-    return document.createElement(element);
-}
-
-function append(parent, el) {
-    return parent.appendChild(el);
-}
+// Fetch Functions
 
 fetch(url)
-    .then((resp) => resp.json())
-    .then(function(data) {
-        let employees = data.results;
-        return employees.map(function(employee) {
-            let li = createNode('li'),
-                img = createNode('img'),
-                h2 = createNode('h2');
-                a = createNode('a');
-                p = createNode('p');
-            li.classList.add('employee');
-            img.src = employee.picture.medium;
-            h2.innerHTML = `${employee.name.first} ${employee.name.last}`;
-            a.innerHTML = `${employee.email}`;
-            p.innerHTML = `${employee.location.city}`;
-            append(li, img);
-            append(li, h2);
-            append(li, a);
-            append(li, p);
-            append(ul, li);
-        })
+    .then(response => response.json())
+    .then(data => {
+        createEmployees(data.results);
     })
-    .catch(function(error) {
-        console.log(error);
-    });
+
+function createEmployees(data) {
+    data.map(result => {
+        const html = `
+            <section class = 'employee-card' index = ${index}>
+            <img class = 'employeeImage' src='${result.picture.large}' alt = 'profile-image'>
+                <h3 class = 'employee-info'>${result.name.first} ${result.name.last}</h3>
+                <p class = 'employee-info'>${result.email}</p>
+                <p class = 'employee-info'>${result.location.city}</p>
+            </section>
+        `
+        index++;
+        list.push(result);
+        employeeList.innerHTML += html;
+    })
+};
+
+function createModalInfo(index) {
+    const person = list[index];
+    const birthday = new Date(person.dob.date).toLocaleString().split(',')[0];
+    const html = `
+      <div class = 'modalContent'>
+        <img class ='modal-employeeImage' src='${person.picture.large}' alt = 'profile-image'>
+        <div class ='modal-info'>
+          <h3>${person.name.first} ${person.name.last}</h3>
+          <p>${person.email}</p>
+          <p>${person.location.city}</p>
+          <hr>
+          <p>${person.cell}</p>
+          <p>${person.location.street.number} ${person.location.street.name} ${person.location.state} ${person.location.postcode}
+          <p>Birthday: ${birthday}
+        </div>
+      </div>
+    `
+    employeeList.appendChild(modal);
+    modal.className = 'modal';
+
+    closeBtn.innerHTML = '&times';
+    closeBtn.id = 'closeBtn';
+
+    modalContainer.innerHTML = html;
+    modalContainer.className = 'modal-container';
+
+    modal.appendChild(modalContainer);
+    modal.appendChild(closeBtn);
+};
+  
+  
+// Helper Functions
+
+function getIndex(e) {
+    if(e.target.className === 'employee-card') {
+        return e.target.getAttribute('index');
+    } else if (e.target.parentNode.className === 'employee-card') {
+        return e.target.parentNode.getAttribute('index');
+    }
+}
+
+function createModal(e) {
+    const modal = document.querySelector('.modal');
+    let personIndex = getIndex(e);
+    createModalInfo(personIndex);
+    return createModal;
+}
+
+function openModal() {
+    modalContainer.style.display = 'block';
+    closeBtn.style.display = 'block';
+}
+
+
+// Event Listeners
+
+employeeList.addEventListener('click', (e) => {
+    if (e.target.className === 'employee-card' || e.target.className === 'employeeImage' || e.target.className === 'employee-info') {
+        createModal(e);
+        openModal();
+    }
+});
+
+closeBtn.addEventListener('click', function closeModal() {
+    modalContainer.style.display = 'none';
+    closeBtn.style.display = 'none';
+});
